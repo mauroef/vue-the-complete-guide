7abdc997a3988abf8c5185d5814b185ade79c578
mauroef <mauroef.dev@gmail.com>
2024-09-01 19:00:09 -0300

    Working with data inside of a vue app

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!',
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: 'Master Vue and build amazing apps!',
       vueLink: 'https://vuejs.org/',
     };
   },
-  methods: {},
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
+  },
 });
 
 app.mount('#user-goal');
