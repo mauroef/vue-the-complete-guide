928ccc39beaec868e01beeccb6daa1a9744acb48
mauroef <mauroef.dev@gmail.com>
2024-09-05 18:50:37 -0300

    Adding a battle log

--- app.js
+++ app.js
@@ -8,14 +8,21 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
-      winner: null
+      winner: null,
+      logMessages: [],
     };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.playerHealth + '%' };
     },
     mayUseSpecialAttack() {
@@ -36,23 +43,26 @@ const app = Vue.createApp({
       } else if (value <= 0) {
         this.winner = 'player';
       }
-    }
+    },
   },
   methods: {
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue)
     },
     specialAttackMonter() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'special attack', attackValue)
       this.attackPlayer();
     },
     healPlayer() {
@@ -63,16 +73,27 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue)
       this.attackPlayer();
     },
     startGame() {
       this.playerHealth = 100;
       this.monsterHealth = 100;
       this.currentRound = 0;
+      this.winner = null;
+      this.logMessages = [];
     },
     surrender() {
-      this.playerHealth = 0;
+      // this.playerHealth = 0;
+      this.winner = 'monster'
     },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value
+      })
+    }
   },
 });
 
