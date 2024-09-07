8d4cedf55315ac648ede78c0f249383da684b9e4
mauroef <mauroef.dev@gmail.com>
2024-09-07 18:43:26 -0300

    Vue app lifecycle

--- app.js
+++ app.js
@@ -15,6 +15,32 @@ const app = Vue.createApp({
       console.log(this.$refs.userText);
     },
   },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  },
 });
 
 app.mount('#app');
+
+setTimeout(() => app.unmount(), 3000);
