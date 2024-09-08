823878d54b26af8dec07d5046ae5cde5edd70e7a
mauroef <mauroef.dev@gmail.com>
2024-09-08 12:13:11 -0300

    Creating a basic vue app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li v-for="friend in friends" :key="friend.id">
+        <h3>{{ friend.name }}</h3>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0123 45678 90',
+          email: 'manuel@localhost.com'
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '0123 45678 91',
+          email: 'julie@localhost.com'
+        }
+      ]
+    }
+  }
+}
+</script>
