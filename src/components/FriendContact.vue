e99ea6c267d843cc9edf548cf39e0c5ba46fcf85
mauroef <mauroef.dev@gmail.com>
2024-09-08 12:39:46 -0300

    Adding styling

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
     <h2>{{ friend.name }}</h2>
-    <button @click="toggleDetails">Show Details</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
       <li><strong>Phone:</strong> {{ friend.phone }}</li>
       <li><strong>Email:</strong> {{ friend.email }}</li>
@@ -14,7 +14,7 @@ export default {
   name: 'FriendContact',
   data() {
     return {
-      detailsAreVisible: true,
+      detailsAreVisible: false,
       friend: {
         id: 'manuel',
         name: 'Manuel Lorenz',
