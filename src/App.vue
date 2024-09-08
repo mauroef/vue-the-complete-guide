5065229563969b2bb45a2aa1db9c08e36b5eb21a
mauroef <mauroef.dev@gmail.com>
2024-09-08 12:30:46 -0300

    Adding a component

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <section>
     <h2>My Friends</h2>
     <ul>
-      <li v-for="friend in friends" :key="friend.id">
-        <h3>{{ friend.name }}</h3>
-      </li>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
     </ul>
   </section>
 </template>
@@ -18,16 +17,16 @@ export default {
           id: 'manuel',
           name: 'Manuel Lorenz',
           phone: '0123 45678 90',
-          email: 'manuel@localhost.com'
+          email: 'manuel@localhost.com',
         },
         {
           id: 'julie',
           name: 'Julie Jones',
           phone: '0123 45678 91',
-          email: 'julie@localhost.com'
-        }
-      ]
-    }
-  }
-}
+          email: 'julie@localhost.com',
+        },
+      ],
+    };
+  },
+};
 </script>
