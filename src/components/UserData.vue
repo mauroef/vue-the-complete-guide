068e3cb4b29b24ab5f2ba5b2072acff698f43d58
mauroef <mauroef.dev@gmail.com>
2024-09-10 20:38:19 -0300

    Assignment 6

---
+++ src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div>
+      <label for="username">Username</label>
+      <input id="username" type="text" placeholder="Enter username" @input="handleUsernameInput" />
+    </div>
+    <div>
+      <label for="age">Age</label>
+      <input id="age" type="text" placeholder="Enter age" @input="handleAgeInput" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ['username-changed', 'age-changed'],
+  methods: {
+    handleUsernameInput(event) {
+      this.$emit('username-changed', event.target.value);
+    },
+    handleAgeInput(event) {
+      this.$emit('age-changed', event.target.value);
+    },
+  },
+};
+</script>
