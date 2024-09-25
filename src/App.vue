2440a48fc6ec9217219885454b0ad6aa5f9d2160
mauroef <mauroef.dev@gmail.com>
2024-09-25 20:00:17 -0300

    First components and props

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resourses/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+        {
+          id: 'stackblitz',
+          title: 'StackBlitz',
+          description: 'Learn to stackblitz...',
+          link: 'https://stackblitz.com',
+        },
+        {
+          id: 'wikipedia',
+          title: 'Wikipedia',
+          description: 'Learn to wikipedia...',
+          link: 'https://wikipedia.org',
+        },
+        {
+          id: 'youtube',
+          title: 'Youtube',
+          description: 'Learn to youtube...',
+          link: 'https://youtube.com',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/learning-resourses/LearningResource.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button @click="$emit('delete-resource', id)">Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link', 'id'],
+};
+</script>
