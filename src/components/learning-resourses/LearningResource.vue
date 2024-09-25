8a00693ff85aa79061f3b66e81dbc00c7e6c09ea
mauroef <mauroef.dev@gmail.com>
2024-09-25 20:47:00 -0300

    Adding a base button

--- src/components/learning-resourses/LearningResource.vue
+++ src/components/learning-resourses/LearningResource.vue
@@ -1,15 +1,16 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button @click="$emit('delete-resource', id)">Delete</button>
+        <!-- <button @click="$emit('delete-resource', id)">Delete</button> -->
+        <base-button mode="flat">Delete</base-button>
       </header>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
