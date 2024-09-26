ef200b4f0e6bbe8170c85b7167c6e5d0a05c3414
mauroef <mauroef.dev@gmail.com>
2024-09-25 23:02:49 -0300

    Adding a form

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,18 @@
 <template>
   <div>
     <the-header title="RememberMe"></the-header>
-    <stored-resources :resources="storedResources"></stored-resources>
+    <the-resources></the-resources>
   </div>
 </template>
 
 <script>
-import StoredResources from './components/learning-resourses/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resourses/TheResources.vue';
 
 export default {
   components: {
-    StoredResources,
     TheHeader,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.com',
-        },
-        {
-          id: 'stackblitz',
-          title: 'StackBlitz',
-          description: 'Learn to stackblitz...',
-          link: 'https://stackblitz.com',
-        },
-        {
-          id: 'wikipedia',
-          title: 'Wikipedia',
-          description: 'Learn to wikipedia...',
-          link: 'https://wikipedia.org',
-        },
-        {
-          id: 'youtube',
-          title: 'Youtube',
-          description: 'Learn to youtube...',
-          link: 'https://youtube.com',
-        },
-      ],
-    };
+    TheResources
   },
 };
 </script>
---
+++ src/components/learning-resourses/TheResources.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <base-card>
+      <base-button
+        @click="setSelectedTab('stored-resources')"
+        :mode="storedResButtonMode"
+        >Stored Resource</base-button
+      >
+      <base-button
+        @click="setSelectedTab('add-resource')"
+        :mode="addResButtonMode"
+        >Add Resource</base-button
+      >
+    </base-card>
+    <component :is="selectedTab"></component>
+  </div>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+        {
+          id: 'stackblitz',
+          title: 'StackBlitz',
+          description: 'Learn to stackblitz...',
+          link: 'https://stackblitz.com',
+        },
+        {
+          id: 'wikipedia',
+          title: 'Wikipedia',
+          description: 'Learn to wikipedia...',
+          link: 'https://wikipedia.org',
+        },
+        {
+          id: 'youtube',
+          title: 'Youtube',
+          description: 'Learn to youtube...',
+          link: 'https://youtube.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
