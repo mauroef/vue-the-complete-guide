7553a4cc94dadb357a52ad75483dc1ce376cc6bf
mauroef <mauroef.dev@gmail.com>
2024-09-29 10:26:34 -0300

    Using v-model on custom components

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,68 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  // data() {
+  //   return {
+  //     activeOption: this.modelValue,
+  //   };
+  // },
+  // computed: {
+  //   activeOption() {
+  //     return this.modelValue
+  //   }
+  // },
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: #a00078;
+}
+
+.active button {
+  color: #a00078;
+}
+
+ul {
+  display: flex;
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+}
+
+li {
+  align-items: center;
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: center;
+  margin: 0 0.25rem;
+  padding: 0.5rem;
+}
+
+button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font: inherit;
+}
+</style>
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,8 +1,18 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: userNameValidity === 'invalid' }"
+    >
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+        @blur="validateInput"
+      />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -83,6 +93,9 @@
       </div>
     </div>
     <div class="form-control">
+      <rating-control v-model="rating"></rating-control>
+    </div>
+    <div class="form-control">
       <input
         type="checkbox"
         id="confirm-terms"
@@ -98,7 +111,12 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue';
+
 export default {
+  components: {
+    RatingControl,
+  },
   data() {
     return {
       userName: '',
@@ -107,6 +125,8 @@ export default {
       interest: [],
       how: '',
       confirm: false,
+      userNameValidity: 'pending',
+      rating: null,
     };
   },
   methods: {
@@ -118,12 +138,22 @@ export default {
         checkboxes: this.interest,
         radios: this.how,
         confirm: this.confirm,
+        rating: this.rating,
       });
       this.userName = '';
       this.userAge = null;
       this.referrer = 'google';
       this.interest = [];
       this.how = '';
+      this.rating = null;
+    },
+    validateInput() {
+      this.userName = this.userName.trim();
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     },
   },
 };
@@ -141,6 +171,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
