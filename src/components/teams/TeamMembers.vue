b85642f908a5c08ca59ffc1111e6d16a0efd988c
mauroef <mauroef.dev@gmail.com>
2024-10-10 18:30:54 -0300

    Scrolling behavior

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -43,6 +43,7 @@ export default {
   },
   created() {
     this.loadTeambers(this.teamId);
+    // console.log(this.$route.query);
   },
   watch: {
     teamId(newId) {
--- src/main.js
+++ src/main.js
@@ -6,22 +6,37 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    // { path: '/', redirect: '/teams' },
+    { path: '/', redirect: '/teams' },
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
-      alias: '/',
-      children: [{ name: 'team-members', path: ':teamId', component: TeamMembers, props: true }],
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter,
+      },
+      // alias: '/',
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
     },
-    { path: '/users', component: UsersList },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    return { left: 0, top: 0 };
+  },
 });
 
 const app = createApp(App);
