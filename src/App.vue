d7d044958249ff53d2b208000ca2e77e4ca0980f
mauroef <mauroef.dev@gmail.com>
2024-10-14 17:38:35 -0300

    Animating a modal

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -30,7 +30,6 @@ export default {
   methods: {
     animateBlock() {
       this.animatedBlock = true; // !this.animateBlock;
-      debugger;
     },
     toggleParagraph() {
       this.paraIsVisible = !this.paraIsVisible;
