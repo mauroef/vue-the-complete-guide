4c9afe6beeeee43074769a65d672b7538e18b54f
mauroef <mauroef.dev@gmail.com>
2024-10-17 08:40:39 -0300

    Creating and using a store

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue';
+import { createStore } from 'vuex';
+
+import App from './App.vue';
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.counter++;
+    },
+  },
+});
+
+const app = createApp(App);
+
+app.use(store);
+
+app.mount('#app');
