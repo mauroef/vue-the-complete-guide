94eefffed320b1281d2cc95b0e571adc86189933
mauroef <mauroef.dev@gmail.com>
2024-10-25 20:41:12 -0300

    Intrucing getters

--- src/main.js
+++ src/main.js
@@ -13,6 +13,24 @@ const store = createStore({
     increment(state) {
       state.counter = state.counter + 2;
     },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 2;
+    },
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    },
   },
 });
 
