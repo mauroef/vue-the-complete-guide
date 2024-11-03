498fc6ca84a034f06b09d20a219a6d208f09e462
mauroef <mauroef.dev@gmail.com>
2024-11-03 12:38:06 -0300

    Adding vuex and coach data

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 
 import router from './router';
+import store from './store';
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 
-createApp(App).mount('#app');
+app.mount('#app');
