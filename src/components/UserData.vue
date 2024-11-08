556c0e682694c6ed5e01f457a728e2bcf1422902
mauroef <mauroef.dev@gmail.com>
2024-11-08 07:44:41 -0300

    Lifecycle hooks

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -6,7 +6,16 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
 
 export default {
   props: ['firstName', 'lastName'],
@@ -16,6 +25,30 @@ export default {
     });
 
     const age = inject('userAge');
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    });
+
+    onMounted(() => {
+      console.log('onMounted');
+    });
+
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    });
+
+    onUpdated(() => {
+      console.log('onUpdated');
+    });
+
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    });
+
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    });
 
     // context.emit('change-age', 30); // this.$emit('change-age', 30);
 
